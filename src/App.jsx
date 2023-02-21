e54b40a8f69848ed7417252fd16150d1ad05f540
Fabrizio Giffi <fabrizio.giffi@gmail.com>
2023-02-21 18:25:32 +0100

    Lab done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import contacts from "./contacts.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [myContacts, setmyContacts] = useState(() => contacts.splice(0, 5));
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1>IronContacts</h1>
+      <div className="buttons">
+        <button
+          onClick={() => {
+            if (contacts.length > 0) {
+              const randActor =
+                contacts[Math.floor(Math.random() * contacts.length)];
+              setmyContacts([randActor, ...myContacts]);
+              const index = contacts.indexOf(randActor);
+              contacts.splice(index, 1);
+              return contacts;
+            }
+          }}
+        >
+          Add random Contact
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button
+          onClick={() => {
+            const sortByPop = myContacts.sort(
+              (a, b) => b.popularity - a.popularity
+            );
+            setmyContacts([...sortByPop]);
+          }}
+        >
+          Sort by popularity
+        </button>
+        <button
+          onClick={() => {
+            const sortByName = myContacts.sort((a, b) => {
+              const nameA = a.name.toUpperCase();
+              const nameB = b.name.toUpperCase();
+              if (nameA < nameB) {
+                return -1;
+              } else if (nameA > nameB) {
+                return 1;
+              }
+            });
+            setmyContacts([...sortByName]);
+          }}
+        >
+          Sort by name
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {myContacts.map((actor) => {
+            return (
+              <tr key={actor.id}>
+                <td>
+                  <div className="proPic grow1">
+                    <img src={actor.pictureUrl} alt={actor.name}></img>
+                  </div>
+                </td>
+                <td>{actor.name}</td>
+                <td>{actor.popularity.toFixed(2)}</td>
+                <td>{actor.wonOscar ? "🏆" : ""}</td>
+                <td>{actor.wonEmmy ? "🌟" : ""}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      const arrAfterDelete = myContacts.filter(
+                        (contact) => contact.id !== actor.id
+                      );
+                      setmyContacts([...arrAfterDelete]);
+                    }}
+                  >
+                    Delete actor
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
